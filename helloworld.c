07f079f542614a8825281a8cfc41efcedbbb03f4
rrrussrrr <rruebner@gmail.com>
2022-08-22 19:11:11 -0400

    added prompt for name

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
+#include<cs50.h>
 
 int main(void) {
+    string answer = get_string("What's yo name? ");
+    printf("hello, %s\n", answer);
 
-    printf("hello world!!!\n");
 }
